70aae4275b807f410313af2dc9bc5e31b33c661e
Jinwoo Jeon <jeonjw1933@gmail.com>
2017-12-11 22:44:17 +0900

    conflict

--- RailRoProject/app/src/main/java/kr/ac/ajou/railroproject/BaseTabFragment.java
+++ RailRoProject/app/src/main/java/kr/ac/ajou/railroproject/BaseTabFragment.java
@@ -1,7 +1,6 @@
 package kr.ac.ajou.railroproject;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
