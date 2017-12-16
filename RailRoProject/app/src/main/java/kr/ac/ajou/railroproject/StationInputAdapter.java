413c8ccecf997838fbca3a7d2fd5bbf243f24de2
gusdnddk111 <gusdnddk111@gmail.com>
2017-12-16 22:55:53 +0900

    코스 등록 푸터 제거

--- RailRoProject/app/src/main/java/kr/ac/ajou/railroproject/StationInputAdapter.java
+++ RailRoProject/app/src/main/java/kr/ac/ajou/railroproject/StationInputAdapter.java
@@ -8,83 +8,51 @@ import android.view.ViewGroup;
 import java.util.ArrayList;
 import java.util.List;
 
-public class StationInputAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-
-    private static final int TYPE_ITEM = 1;
-    private static final int TYPE_FOOTER = 2;
-
-    private int stationCount;
+public class StationInputAdapter extends RecyclerView.Adapter<StationInputViewHolder> {
+    
     private List<Station> stationList = new ArrayList<Station>();
     private List<StationInputViewHolder> holderList = new ArrayList<>();
 
-    public StationInputAdapter(List<Station> stationList){
+    public StationInputAdapter(List<Station> stationList) {
         this.stationList = stationList;
     }
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public StationInputViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
         View itemView;
 
-        if(viewType == TYPE_ITEM){
-            itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_station_add, parent, false);
-            StationInputViewHolder viewHolder =  new StationInputViewHolder(itemView);
-            holderList.add(viewHolder);
-            return viewHolder;
-        }else if (viewType == TYPE_FOOTER){
-            itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_station_add_footer, parent, false);
+        itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_station_add, parent, false);
+        StationInputViewHolder viewHolder = new StationInputViewHolder(itemView);
+        holderList.add(viewHolder);
+        return viewHolder;
 
-            return new StationInputFooterViewHolder(itemView);
-        }
-
-        return null;
 
     }
 
-    @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        if (holder instanceof StationInputViewHolder) {
-            //뷰 타입이 아이템일 경우
-            //StationInputViewHolder item_holder = (StationInputViewHolder) holder;
-        } else if (holder instanceof StationInputFooterViewHolder) {
-            //뷰타입이 푸터 생성하기 버튼일 경우
 
-        }
+    @Override
+    public void onBindViewHolder(StationInputViewHolder holder, int position) {
+
     }
 
     @Override
     public int getItemCount() {
-        return stationList.size()+1;
+        return stationList.size();
     }
 
-    public void addInputView(){
-        stationCount++;
-    }
 
-    //뷰타입 정하기
-    @Override
-    public int getItemViewType(int position) {
-        if (isPositionFooter(position)) {
-            return TYPE_FOOTER;
-        }
-        return TYPE_ITEM;
-    }
-
-    public List<Station> getStationList(){
+    public List<Station> getStationList() {
         return stationList;
     }
 
     public ArrayList<Station> finish() {
         ArrayList<Station> tempStationList = new ArrayList<>();
-        for(StationInputViewHolder holder : holderList){
+        for (StationInputViewHolder holder : holderList) {
             tempStationList.add(holder.getStation());
         }
 
         return tempStationList;
     }
 
-    //true가 반환되면 리스트의 끝임을 알수있다.
-    private boolean isPositionFooter(int position) {
-        return position == stationList.size();
-    }
 }
